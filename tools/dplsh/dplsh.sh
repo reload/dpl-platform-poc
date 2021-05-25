881daf3ffa90e2930a1918dc43f638a06bd21f2c
Mads H. Danquah <mads@danquah.dk>
2021-05-25 09:30:39 +0200

    Make it possible to run dplsh non-interactive

--- tools/dplsh/dplsh.sh
+++ tools/dplsh/dplsh.sh
@@ -204,10 +204,15 @@ for key in "${!docker_creds[@]}"; do
   ADDITIONAL_ARGS+=(-e "${key}=${docker_creds[$key]}")
 done
 
+# We run in interactive mode unless if we're in DPLSH_NON_INTERACTIVE.
+if [[ -z "${DPLSH_NON_INTERACTIVE:-}" ]]; then
+  ADDITIONAL_ARGS+=(-i)
+fi
+
 docker run --hostname=dplsh \
     --rm \
     "${ADDITIONAL_ARGS[@]}" \
-    -ti \
+    -t \
     -v "${HOME}/.azure:/home/dplsh/.azure-host" \
     -v "${SHELL_ROOT}:/home/dplsh/host_mount" \
     -w "/home/dplsh/host_mount/${CHDIR}" \
